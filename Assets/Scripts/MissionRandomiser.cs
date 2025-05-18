e8a52bc9db34c941efa8763d474407f514b0ecef
ELLIS WOODS <ew280189@falmouth.ac.uk>
2025-05-18 04:42:51 +0100

    Made the first prototype for randomiser

--- Assets/Scripts/MissionRandomiser.cs
+++ Assets/Scripts/MissionRandomiser.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using System.Collections;
+using Unity.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
 
 public class MissionRandomiser : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public const string missionsFileName = "MissionList.txt";
+    public const string flavourFileName = "FlavourTextList.txt";
+
+
+
+    private string persistentMissionsFilePath;
+    private string persistentFlavourFilePath;
+    public bool MissionsFound { get; private set; }
+    private string[] missions;
+    private string[] flavour;
+
+    [SerializeField]
+    TextMeshProUGUI resultText,flavourText;
+
+
+    private void Awake()
     {
-        
+        persistentMissionsFilePath = (Application.persistentDataPath + "/" + missionsFileName);
+        persistentFlavourFilePath = (Application.persistentDataPath + "/" + flavourFileName);        
+    }
+    private void LoadFlavourText()
+    {
+        if (!File.Exists(persistentFlavourFilePath))
+        {
+            Debug.LogError("File not found in Persistent data path: " + persistentFlavourFilePath);
+            return;
+        }
+
+        string text = File.ReadAllText(persistentFlavourFilePath);
+        flavour = text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+    }
+    private void LoadMissions()
+    {
+        if (!File.Exists(persistentMissionsFilePath))
+        {
+            Debug.LogError("File not found in Persistent data path: " + persistentMissionsFilePath);
+            MissionsFound = false;
+            return;
+        }
+
+        string text = File.ReadAllText(persistentMissionsFilePath);
+        missions = text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        MissionsFound = true;
+    }
+    public void DisplayMissions()
+    {
+        LoadMissions();
+        LoadFlavourText();
+        if (MissionsFound)
+        {
+            string flavourMessage = flavour[Random.Range(0, flavour.Length)] ?? "Install the flavour texts you tasteless bastard" ;
+            flavourText.text = flavourMessage;
+
+            string resultMessage = missions[Random.Range(0, missions.Length)] ?? "This literally should not be possible to see lol";
+            resultText.text = resultMessage;
+        }
+        else
+        {
+            flavourText.text = "TRIED TO SPIN WHEN NO FILES ARE FOUND";
+            resultText.text = "TRIED TO SPIN WHEN NO FILES ARE FOUND";
+        }
     }
 }
